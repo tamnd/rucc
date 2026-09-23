8cbbbc7df37d586df22212b6a4ac5c2d1757c9a2
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2026-09-23 17:18:36 +0700

    Release 0.10.78 (#1723)

--- crates/rucc-unpack/src/lib.rs
+++ crates/rucc-unpack/src/lib.rs
@@ -44,7 +44,7 @@
 //!
 //! [`inflate`]: mod@inflate
 
-#![doc(html_root_url = "https://docs.rs/rucc-unpack/0.10.77")]
+#![doc(html_root_url = "https://docs.rs/rucc-unpack/0.10.78")]
 
 pub mod cab;
 pub mod cfb;
