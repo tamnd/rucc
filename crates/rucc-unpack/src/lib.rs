9245e330a6c59c0551ead642c0e5cfc2ad218bba
tamnd <1218621+tamnd@users.noreply.github.com>
2026-09-26 11:15:09 +0700

    Release 0.11.10

--- crates/rucc-unpack/src/lib.rs
+++ crates/rucc-unpack/src/lib.rs
@@ -44,7 +44,7 @@
 //!
 //! [`inflate`]: mod@inflate
 
-#![doc(html_root_url = "https://docs.rs/rucc-unpack/0.11.9")]
+#![doc(html_root_url = "https://docs.rs/rucc-unpack/0.11.10")]
 
 pub mod cab;
 pub mod cfb;
